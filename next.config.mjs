c5f2f694399ffa5c9a170675cf58c5e27e8c8fd3
zhemmmzh <86453687+zhemmmzh@users.noreply.github.com>
2025-11-21 01:00:43 +0800

    Remove basePath configuration from next.config.mjs
    Removed basePath configuration for production.

--- next.config.mjs
+++ next.config.mjs
@@ -8,9 +8,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // 配置 BasePath
-  // 如果你的仓库名是 my-blog，则填写 "/my-blog"
-  basePath: process.env.NODE_ENV === 'production' ? "/my-blog" : "",
+
 };
 
 export default withNextIntl(nextConfig);
