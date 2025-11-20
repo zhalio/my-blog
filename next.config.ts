07c4a82bebdb41c45de5a3def1e4ec1fa42b53a8
emmm <1992107794@qq.com>
2025-11-20 22:23:40 +0800

    fix: explicitly specify next-intl request config path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
