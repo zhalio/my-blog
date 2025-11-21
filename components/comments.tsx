7fd047e3f4c5bc42724e1290f25e97e3930378aa
emmm <1992107794@qq.com>
2025-11-21 08:20:56 +0800

    fix: update giscus repo and category ids

--- components/comments.tsx
+++ components/comments.tsx
@@ -12,9 +12,9 @@ export function Comments() {
       <Giscus
         id="comments"
         repo="zhemmmzh/my-blog"
-        repoId="R_kgDON2_e_w"
+        repoId="R_kgDOQZTj3g"
         category="Announcements"
-        categoryId="DIC_kwDON2_e_84Cm3_l"
+        categoryId="DIC_kwDOQZTj3s4CyBgh"
         mapping="pathname"
         strict="0"
         reactionsEnabled="1"
