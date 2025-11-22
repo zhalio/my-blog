7eb6096f2c7dd8910c535dd332d9777020de6ab6
emmm <1992107794@qq.com>
2025-11-22 16:48:14 +0800

    feat: add stats and tags system

--- app/api/stats/route.ts
+++ app/api/stats/route.ts
@@ -4,8 +4,8 @@ import { Redis } from '@upstash/redis';
 export const runtime = 'edge';
 
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL || process.env.KV_REST_API_URL || '',
-  token: process.env.UPSTASH_REDIS_REST_TOKEN || process.env.KV_REST_API_TOKEN || '',
+  url: process.env.UPSTASH_REDIS_REST_URL || process.env.KV_REST_API_URL || process.env.STORAGE_URL || '',
+  token: process.env.UPSTASH_REDIS_REST_TOKEN || process.env.KV_REST_API_TOKEN || process.env.STORAGE_TOKEN || '',
 });
 
 export async function GET(req: NextRequest) {
